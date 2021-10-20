6114f894db48fffb4f016cde0d4207c456cd360e
Tigran Harutyunyan <harutyunyan.tigran.v@gmail.com>
2021-10-20 02:23:15 -0400

    Update populate.sql

--- sql_files/populate.sql
+++ sql_files/populate.sql
@@ -1,8 +1,4 @@
--- -- Uncomment when db doesn't exist
-CREATE DATABASE exploitdb
-   ENCODING 'UTF8';
--- --
-CREATE TABLE IF NOT EXISTS exploits (
-    id integer primary key,
-    ip VARCHAR(60),
-    Country TEXT);
+-- Uncomment when db doesn't exist
+CREATE DATABASE exploitdb ENCODING 'UTF8';
+
+CREATE TABLE IF NOT EXISTS exploits (id integer primary key,ip VARCHAR(60), Country TEXT);
