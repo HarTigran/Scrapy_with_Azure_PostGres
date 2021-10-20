49cc9e2b6637cc331b5bfc70f272df53d8a3c3cb
Tigran Harutyunyan <harutyunyan.tigran.v@gmail.com>
2021-10-20 02:35:33 -0400

    Update populate.sql

--- sql_files/populate.sql
+++ sql_files/populate.sql
@@ -2,3 +2,6 @@
 -- CREATE DATABASE exploitdb ENCODING 'UTF8';
 
 -- CREATE TABLE IF NOT EXISTS exploits (id integer primary key,ip VARCHAR(60), Country TEXT);
+
+ALTER TABLE exploits
+    ALTER COLUMN id SET DEFAULT nextval('ip_id_seq');
