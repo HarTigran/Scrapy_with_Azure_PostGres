29b8bb26151b967cdaed5f17de3afe052127ce36
Tigran Harutyunyan <harutyunyan.tigran.v@gmail.com>
2021-10-20 21:37:00 -0400

    Update populate.sql

--- sql_files/populate.sql
+++ sql_files/populate.sql
@@ -1,5 +1,5 @@
 -- Uncomment when db doesn't exist
-DROP TABLE badips CASCADE;
+DROP TABLE  IF EXISTS badips CASCADE;
 DROP DATABASE IF EXISTS badiplistdb;
 CREATE DATABASE badiplistdb ENCODING 'UTF8';
 CREATE TABLE IF NOT EXISTS badips (ip VARCHAR(60), Country TEXT);
